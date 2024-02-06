67c681d63b228f0d677238c4f7e259e0ad5746de
Pineda02207 <93058689+Pineda02207@users.noreply.github.com>
2024-02-05 23:28:48 -0600

    Add files via upload

---
+++ CalcArea01.py
@@ -0,0 +1,7 @@
+
+lado = int(input("Cual es la medida del lado del cuadrado"))
+area = lado*lado
+perimetro = lado*4
+
+print("El area de su cuadrado es : ", area)
+print("El perimetro de su cudrado", perimetro)
